d4eac75da88f82ce464db5d4a81075ba64a1a826
Florian Neuß <floneuss99@gmail.com>
2024-09-18 18:45:39 +0200

    generate api client

---
+++ src/api/models/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Claimsdata';
+export * from './ErrorList';
+export * from './ErrorListErrorsInner';
+export * from './Person';
+export * from './Policyholder';
+export * from './VehicleDriver';
+export * from './VehicleDriverDamagedCarImagesInner';
+export * from './VehicleDriverDamagedWindowImagesInner';
+export * from './Witness';
